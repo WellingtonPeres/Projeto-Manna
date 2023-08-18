a8aaf164c94727312d080fda7cbadd185dd9bee0
Wellington Peres <wellingtonpv.100@gmail.com>
2023-08-18 18:37:16 -0300

    Alterações no Script de botão
    Modifiquei o script do botão de analisar as questões

--- Assets/Scritps/Minigame/DragAndDrop/Button.cs
+++ Assets/Scritps/Minigame/DragAndDrop/Button.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-<<<<<<< Updated upstream
-=======
-
-
->>>>>>> Stashed changes
     public void UpdateArray()
     {
         MinigameManager.instance.UpdateArray();
