3159a4d5d9c991c18333e2d39fa71f5dc6185323
Wellington Peres <wellingtonpv.100@gmail.com>
2023-08-18 18:23:57 -0300

    Login Firebase - Parte 8
    Fiz um sistema de login com o Firebase, já posso cadastrar uma pessoa e verificar se ela já tem cadastro. OBS.: Ainda não trocas de fase

--- Assets/Scritps/Minigame/DragAndDrop/Button.cs
+++ Assets/Scritps/Minigame/DragAndDrop/Button.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
+<<<<<<< Updated upstream
+=======
 
+
+>>>>>>> Stashed changes
     public void UpdateArray()
     {
         MinigameManager.instance.UpdateArray();
