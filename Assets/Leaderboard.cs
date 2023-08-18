b4206903614a3488dc85d10a4d8d4bcd8ed08336
Wellington Peres <wellingtonpv.100@gmail.com>
2023-08-18 18:24:16 -0300

    Login Firebase - Parte 9
    Fiz um sistema de login com o Firebase, já posso cadastrar uma pessoa e verificar se ela já tem cadastro. OBS.: Ainda não trocas de fase

---
+++ Assets/Leaderboard.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LootLocker.Requests;
+
+public class Leaderboard : MonoBehaviour
+{
+    int leaderboardID = 16972;
+
+    void Start()
+    {
+        
+    }
+
+    public IEnumerator SubmitScoreRoutine(int scoreToUpload)
+    {
+        bool done = false;
+        string playerID = PlayerPrefs.GetString("PlayerID");
+        LootLockerSDKManager.SubmitScore(playerID, scoreToUpload, leaderboardID, (response) =>
+        {
+            if (response.success)
+            {
+                Debug.Log("Successfully uploaded score");
+                done = true;
+            }
+            else
+            {
+                Debug.Log("Failed" + response.Error);
+                done = true;
+            }
+        });
+        yield return new WaitWhile(() => done == false);
+    }
+}
